157a6529d680e6acfa510226376e6bc882d55b3d
lazizbekshodmonov <lazizbekshodmonov001@gmail.com>
2023-02-22 11:33:45 +0500

    bug fixing

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const serverBaseUrl = 'https://47.254.127.151/'
+const serverBaseUrl = 'http://47.254.127.151/'
 // const localBaseUrl = "http://127.0.0.1:8000/"
 const userAxios = axios.create({
     baseURL: serverBaseUrl,
